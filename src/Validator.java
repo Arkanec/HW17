32dee34355b5d4fa6e0f2e5479d5bd8d7c688a4e
PC <black-angel-eremin@yandex.ru>
2023-12-15 23:54:46 +0300

    Мой коммит

--- src/Validator.java
+++ src/Validator.java
@@ -1,24 +1,23 @@
 public class Validator {
 
-    private static final String LEGAL_CHARACTERS =
-            "qwertyuiopasdfghjklzxcvbnm1234567890_";
+    private static final String LEGAL_CHARACTERS = "qwertyuiopasdfghjklzxcvbnm1234567890_";
 
     private static final int MAX_LENGTH = 20;
 
     public static void validate(String login, String password, String confirmPassword) {
 
-    for (int i = 0; i < login.length(); i++) {
-        if(LEGAL_CHARACTERS.indexOf(login.charAt(i)) == -1) {
-            throw new WrongLoginException("Логин содержит недопустимые символы");
+        for (int i = 0; i < login.length(); i++) {
+            if (LEGAL_CHARACTERS.indexOf(login.charAt(i)) == -1) {
+                throw new WrongLoginException("Логин содержит недопустимые символы");
+            }
         }
-    }
 
-    if (login.length() > MAX_LENGTH) {
-        throw new WrongLoginException("Длина логина превышает максимум");
-    }
+        if (login.length() > MAX_LENGTH) {
+            throw new WrongLoginException("Длина логина превышает максимум");
+        }
 
         for (int i = 0; i < password.length(); i++) {
-            if(LEGAL_CHARACTERS.indexOf(password.charAt(i)) == -1) {
+            if (LEGAL_CHARACTERS.indexOf(password.charAt(i)) == -1) {
                 throw new WrongPasswordException("Пароль содержит недопустимые символы");
             }
         }
